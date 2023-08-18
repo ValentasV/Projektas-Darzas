f2abaec58a8d757c00462df4a7399b445124f0f9
ValentasV <rroebd@gmail.com>
2023-08-18 20:29:24 +0300

    pridėtas requirements failas

--- Projektas_Daržininkams/vartotojai/apps.py
+++ Projektas_Daržininkams/vartotojai/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class VartotojoProfilisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vartotojo_profilis'
+    name = 'vartotojai'
